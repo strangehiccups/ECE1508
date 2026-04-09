dd5c9163f048a79f67e02ee03c6b944f0f1fc46a
Anubhav Sharma <67965197+anubhavsharma515@users.noreply.github.com>
2026-04-08 20:28:57 -0400

    Final update to README

--- Code/config.py
+++ Code/config.py
@@ -1,14 +1,7 @@
 from enum import Enum
 from transformers import Wav2Vec2CTCTokenizer
 
-
-class TemporalNetwork(Enum):
-    GRU = 1
-    GRU_BIDIRECTIONAL = 2
-    LSTM = 3
-    CONFORMER = 4
-
-
+# DATASET URLS
 LJSPEECH_URL = "https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2"
 
 # Language model URLs
@@ -21,15 +14,18 @@ BATCH_SIZE  = 32
 LEARNING_RATE = 3e-4
 NUM_EPOCHS = 20
 
+# DATALOADER CONFIG
 NUM_WORKERS = 0
 PREFETCH_FACTOR = 2
 PIN_MEMORY = True
 
+# AUDIO FEATURE CONFIG
 HOP_LENGTH = 256
 N_FFT = 512
 N_MELS = 80
 SAMPLE_RATE = 16000
 
+# SAVE PATHS
 SAVE_MODEL_PATH = "model.pth"
 SAVE_BEST_MODEL_PATH = "model_best.pth"
 SAVE_HISTORY_PATH = "history.h5"
@@ -44,6 +40,7 @@ HISTORY_KEYS = [
     "val_time"
 ]
 
+# TOKENIZER CONFIG
 TOKENIZER = Wav2Vec2CTCTokenizer.from_pretrained("facebook/wav2vec2-base")
 BLANK_TOKEN_ID = TOKENIZER.pad_token_id
 
